ced2a341966b1d4c7961481fa18d86622b4d2cab
Amer Sikira <amersikira@Amers-MacBook-Air.local>
2026-09-17 23:07:34 +0200

    Remove Viber link trailing slash

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const viberChatUrl = 'viber://chat?number=387644421927/';
+  const viberChatUrl = 'viber://chat?number=387644421927';
   const body = document.body;
   const navToggle = document.querySelector('.nav-toggle');
   const nav = document.querySelector('.site-nav');
